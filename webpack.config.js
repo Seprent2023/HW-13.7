f20598b332bfb5d6a8f7c4449ac1d152759fb246
Seprent2023 <125795015+Seprent2023@users.noreply.github.com>
2024-11-12 16:52:43 +0700

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,18 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    entry: './src/js/index.js',
-    devServer: {
-        static: path.resolve(__dirname, 'dist'),
-        open: true,
-        hot: true,
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [
             {
-                test: /\.(scss|css|sass)$/,
-                use: ["style-loader", "css-loader", "sass-loader",]
-            }
-        ]
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, 'dist'),
-    }
-}
+};
